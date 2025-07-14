885f1fedea3d7d2f7ccdf3220ce118fe09d4afae
Amir Mohammad Qarebaq (BlackGarden) <95334969+Qarebaq@users.noreply.github.com>
2025-07-14 17:43:26 +0330

    Create DFS.py

---
+++ DFS.py
@@ -0,0 +1,21 @@
+def dfs(graph, start):
+    visited = set()
+    stack = [start]
+
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            print(vertex)
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
+
+dfs(graph, 'A')
